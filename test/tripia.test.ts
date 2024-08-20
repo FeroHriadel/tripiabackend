4ec210c85b53e9ab0779d37c1158111040ae18c1
Fero Hriadel <ferdinand.hriadel@gmail.com>
2024-08-20 10:29:41 +0200

    Initial commit

---
+++ test/tripia.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Tripia from '../lib/tripia-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tripia-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Tripia.TripiaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
